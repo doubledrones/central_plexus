314eff183288de81467b94d0ad0d69f1565611a2
Marcin Michalowski and Marcin Nowicki <core+h13ronim+pr0d1r2@doubledrones.com>
2010-09-28 19:18:32 +0200

    Initial setup file.

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+ln -s ~/projects/central_plexus/.bash_profile ~/
+source ~/.bash_profile
+
+cp ~/projects/central_plexus/.gitconfig ~/
+echo "git config user.name:"
+read GIT_NAME
+echo "git config user.email:"
+read GIT_EMAIL
+git config --global user.name "$GIT_NAME"
+git config --global user.email "$GIT_EMAIL"
