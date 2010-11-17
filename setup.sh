3265c9bb06ed8940f355d72175d2063b4e947f0d
Marcin Nowicki <pr0d1r2@gmail.com>
2010-11-17 10:49:08 +0100

    Remove unnecessary git config.
    Config itself moved to sync script.

--- setup.sh
+++ setup.sh
@@ -15,12 +15,6 @@ rvm update --edge
 rvm reload
 
 cp ~/projects/central_plexus/.gitconfig ~/
-echo "git config user.name:"
-read GIT_NAME
-echo "git config user.email:"
-read GIT_EMAIL
-git config --global user.name "$GIT_NAME"
-git config --global user.email "$GIT_EMAIL"
 
 if [ ! -L ~/.gemrc ]; then
   ln -s ~/projects/central_plexus/.gemrc ~/
