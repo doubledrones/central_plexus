3d8414ad38a43a8a87ea8cc62baee5fa584282e0
Marcin Nowicki <pr0d1r2@gmail.com>
2011-05-04 13:42:54 +0200

    Correct typo.

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ if [ ! -x $HOME/.rvm/scripts/rvm ]; then
   RVM_INSTALL=/tmp/rvm-install-`date "+%Y%m%d%H%M%S"`
   curl https://rvm.beginrescueend.com/install/rvm -o $RVM_INSTALL
   chmod 700 $RVM_INSTALL
-  $RVM_INSTALL --version lastest
+  $RVM_INSTALL --version latest
   rm -f $RVM_INSTALL
 fi
 
