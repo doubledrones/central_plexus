3baeb57fcfe6dad1171f6e1fc3d20246e5b8701c
Marcin Nowicki <pr0d1r2@gmail.com>
2011-07-05 08:41:55 +0200

    Ignore certificate when download RVM installer.

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ export GEM_HOME="$HOME/.gem/ruby/1.8"
 
 if [ ! -x $HOME/.rvm/scripts/rvm ]; then
   RVM_INSTALL=/tmp/rvm-install-`date "+%Y%m%d%H%M%S"`
-  curl https://rvm.beginrescueend.com/install/rvm -o $RVM_INSTALL
+  curl https://rvm.beginrescueend.com/install/rvm -o $RVM_INSTALL -k
   chmod 700 $RVM_INSTALL
   $RVM_INSTALL --version latest
   rm -f $RVM_INSTALL
