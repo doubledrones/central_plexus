fbc6380d245d1ae429c4a1db6fc083743dd75765
Marcin Nowicki <pr0d1r2@gmail.com>
2011-07-06 09:46:45 +0200

    Start setup script for Mac OS X Tiger.
    Installs RVM.

---
+++ setup-tiger.sh
@@ -0,0 +1,27 @@
+
+#!/bin/sh -ev
+
+D_R=`cd \`dirname $0\` ; pwd -P`
+
+DEFAULT_RVM_RUBY="ruby-1.9.2"
+
+export GEM_HOME="$HOME/.gem/ruby/1.8"
+
+if [ ! -x $HOME/.rvm/scripts/rvm ]; then
+  RVM_INSTALL=/tmp/rvm-install-`date "+%Y%m%d%H%M%S"`
+  curl https://rvm.beginrescueend.com/install/rvm -k | \
+    sed -e '6s/^/#/g' | \
+    sed -e '166,170s/^/#/g' | \
+    cat > $RVM_INSTALL
+  chmod 700 $RVM_INSTALL
+  $RVM_INSTALL --version latest
+  rm -f $RVM_INSTALL
+  cd $HOME/.rvm/src
+  cd `ls`
+  cp scripts/install scripts/install.tiger
+  cat scripts/install | \
+    sed -e '6,13s/^/#/g' | \
+    cat > scripts/install.tiger
+  mv scripts/install.tiger scripts/install
+  ./scripts/install --prefix "$HOME" --path "$HOME/.rvm"
+fi
