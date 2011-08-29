02a535a11d11e0c1ac46329a6f7e7b6be667db85
Marcin Nowicki <pr0d1r2@gmail.com>
2011-08-29 20:13:25 +0200

    Make submodule_autoupdate to handle not git submodules.

--- submodule_autoupdate.sh
+++ submodule_autoupdate.sh
@@ -3,6 +3,19 @@
 cd `dirname $0`
 
 git pull
+if [ ! -d applicage/.git ]; then
+  if [ -d applicage ]; then
+    rm -rf applicage
+  fi
+  git clone git@github.com:/doubledrones/applicage.git
+fi
+if [ ! -d dotmatrix/.git ]; then
+  if [ -d dotmatrix ]; then
+    rm -rf dotmatrix
+  fi
+  git clone git@github.com:/doubledrones/dotmatrix.git
+fi
+
 cd applicage
 git checkout master
 git pull
