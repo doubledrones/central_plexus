a815a6614d04837d13d29e8cc5eca5bbd18fc06e
Marcin Michalowski <h13ronim@gmail.com>
2012-05-12 22:10:48 +0200

    Update rvm install script

--- rvm_install.sh
+++ rvm_install.sh
@@ -1,9 +1,5 @@
-#!/bin/sh -ev
+#!/bin/bash -ev
 
 if [ ! -x $HOME/.rvm/scripts/rvm ]; then
-  RVM_INSTALL=/tmp/rvm-install-`date "+%Y%m%d%H%M%S"`
-  curl https://rvm.beginrescueend.com/install/rvm -o $RVM_INSTALL -k
-  chmod 700 $RVM_INSTALL
-  $RVM_INSTALL $@
-  rm -f $RVM_INSTALL
+  curl -L get.rvm.io | bash -s stable
 fi
